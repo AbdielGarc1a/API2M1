15b7cf562b5b38e1ca3e7cb0afe01c188ef1e1e6
AbdielGE <86158145+AbdielGarc1a@users.noreply.github.com>
2022-04-05 15:48:50 -0600

    Add files via upload

---
+++ Appcore/Service/WeatherService.cs
@@ -0,0 +1,42 @@
+﻿
+using Appcore.Interface;
+using Domain.Entities;
+using Domain.Entities.WeatherInfo;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appcore.Service
+{
+    public class WeatherService:IWeatherService
+    {
+        private IWeatherInfo weatherInfo;
+
+        public WeatherService(IWeatherInfo weatherInfo)
+        {
+            this.weatherInfo = weatherInfo;
+        }
+
+        public DateTime convertToDateTime(long milisegundos)
+        {
+            return weatherInfo.convertToDateTime(milisegundos);
+        }
+
+        public string GetImageLocation(Weather w)
+        {
+            return weatherInfo.GetImageLocation(w);
+        }
+
+        public Root GetWeather(string ciudad)
+        {
+            return weatherInfo.GetWeather(ciudad);
+        }
+
+        public ForecastInfo GetWeatherForecast()
+        {
+            return weatherInfo.GetWeatherForecast();
+        }
+
+    }
+}
