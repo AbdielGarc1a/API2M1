2dbbe99b0cb50d814f6a77702362d3d87523f17c
AbdielGE <86158145+AbdielGarc1a@users.noreply.github.com>
2022-04-05 11:51:30 -0600

    Add files via upload

---
+++ API_2M1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Appcore.Interface;
+using Appcore.Service;
+using Autofac;
+using Domain.Interface;
+using Infraestructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace API_2M1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<JsonWeatherRepository>().As<IWeatherInfo>();
+            builder.RegisterType<WeatherService>().As<IWeatherService>();
+            var container = builder.Build();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(container.Resolve<IWeatherService>()));
+        }
+    }
+}
